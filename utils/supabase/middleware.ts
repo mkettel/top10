0cb7c5ae9afe3834ee5db65ebab80d2adc01b4b2
mkettel <mattkettelkamp@gmail.com>
2025-04-02 10:22:12 -0600

    updated to redirect to homepage if not logged in

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,11 +40,12 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/') // Add this line to exclude the homepage
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, redirect to homepage instead of login
     const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    url.pathname = '/' // Change to homepage
     return NextResponse.redirect(url)
   }
 
