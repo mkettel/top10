87b5e80c8a30be4a7ef3aaacd7e1289fe8482963
mkettel <mattkettelkamp@gmail.com>
2025-04-02 10:30:17 -0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col bg-offwhite items-center justify-center">
       <div className="flex flex-col justify-center items-center gap-6">
-        <h1 className='flex gap-2 font-outfit text-6xl'><span>The</span><span>Top 10</span><span>Game</span></h1>
+        <h1 className='flex flex-col sm:flex-row gap-2 font-outfit text-6xl'><span>The</span><span>Top 10</span><span>Game</span></h1>
         <Link href="/login" className='flex w-fit items-center gap-2 text-2xl font-outfit text-offwhite bg-new-blue px-4 py-2 rounded-md'>
           Play Now
         </Link>
